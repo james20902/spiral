14647ca59739c52e9e3f14f362835515ee58f13b
unknown <james20902@gmail.com>
2019-08-29 16:06:12 -0400

    more stuff on SI units

--- src/main/java/frc/lib/utility/SystemClock.java
+++ src/main/java/frc/lib/utility/SystemClock.java
@@ -2,7 +2,6 @@ package frc.lib.utility;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.hal.HALUtil;
-import java.util.concurrent.TimeUnit;
 
 public class SystemClock {
 
@@ -10,18 +9,18 @@ public class SystemClock {
     //this class is very very broken right now, the conversions are wrong
 
     //micro
-    public static double getSystemTime(TimeUnit unit){
-        return TimeUnit.MICROSECONDS.convert(HALUtil.getFPGATime(), unit);
+    public static double getSystemTime(Units unit){
+        return Units.MICRO.convert(HALUtil.getFPGATime(), unit);
     }
     public static double getSystemTime(){
-        return getSystemTime(TimeUnit.MILLISECONDS);
+        return getSystemTime(Units.MILLI);
     }
 
-    public static double getMatchTime(TimeUnit unit){
-        return TimeUnit.MILLISECONDS.convert((long)HAL.getMatchTime(), unit);
+    public static double getMatchTime(Units unit){
+        return Units.MILLI.convert((long)HAL.getMatchTime(), unit);
     }
     public static double getMatchTime(){
-        return getMatchTime(TimeUnit.SECONDS);
+        return getMatchTime(Units.BASE);
     }
 
 }
--- src/main/java/frc/lib/utility/Units.java
+++ src/main/java/frc/lib/utility/Units.java
@@ -2,20 +2,27 @@ package frc.lib.utility;
 
 public enum Units {
     //SI units conversions
-    GIGA(1000000000),
-    MEGA(1000000),
-    KILO(1000),
-    CENTI(0.01),
-    MILLI(0.001),
-    MICRO(0.000001),
-    NANO(0.000000001),
-    PICO(0.000000000001);
+    GIGA(9, 3),
+    MEGA(6, 2),
+    KILO(3, 1),
+    CENTI(-2, -1),
+    BASE(1, 0),
+    MILLI(-3, -2),
+    MICRO(-6, -3),
+    NANO(-9, -4),
+    PICO(-12, -5);
 
-    private double multipler;
+    private int multipler;
+    private int scale;
 
-    private Units(double multiplier){
+    private Units(int multiplier, int scale){
         this.multipler = multiplier;
     }
 
-    
+    public double convert(double measure, Units unit){
+        double multiplier = this.scale > unit.scale ? 0 : 1;
+        return measure * unit.multipler;
+    }
+
+
 }

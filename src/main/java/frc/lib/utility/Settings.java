c48597ed5db04750fecf9a436fa5b1944a68a0be
bluetangkid <bluetangkid@gmail.com>
2019-11-11 16:45:28 -0500

    Some settings work which is hopefully working enough to stay for a while

---
+++ src/main/java/frc/lib/utility/Settings.java
@@ -0,0 +1,53 @@
+package frc.lib.utility;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import frc.lib.output.drivebase.DriveSystem;
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+public class Settings {
+    private static Settings instance;
+
+    public float maxVelocity;
+    public float ticksPerRevolutionWB;
+    public float wheelDiameter;
+    public float[][] deadzones;
+    public String drivetrain;
+    private Class<DriveSystem> driveType;
+
+    private Settings(){
+        maxVelocity = 1;
+        ticksPerRevolutionWB = -1;
+        wheelDiameter = -1;
+        instance = this;
+        deadzones = new float[1][1];
+        load();
+    }
+
+    public static Settings getInstance(){
+        if(instance == null) instance = new Settings();
+        return instance;
+    }
+
+    public void load(){
+        Constructor constructor = new Constructor(Settings.class);
+        TypeDescription description = new TypeDescription(Settings.class);
+        constructor.addTypeDescription(description);
+        Yaml yaml = new Yaml(constructor);
+        instance = (Settings) yaml.load(Filesystem.getDeployDirectory().getAbsolutePath()+"/robot.settings");
+        try {
+            for (Class c : ClassFinder.getClasses("frc.lib.output.drivebase")) {
+                if(c.toString().toUpperCase().equals(drivetrain.toUpperCase())) {
+                    driveType = c;
+                }
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Class<DriveSystem> getDriveType(){
+        return driveType;
+    }
+}

1a8d41bf3c2b5b08c5d2991a88af1ea3399849d9
bluetangkid <bluetangkid@gmail.com>
2019-11-13 19:09:19 -0500

    Using Settings.java in DriveSystem.

--- src/main/java/frc/lib/output/drivebase/ArcadeDrive.java
+++ src/main/java/frc/lib/output/drivebase/ArcadeDrive.java
@@ -1,10 +1,14 @@
 package frc.lib.output.drivebase;
 
+import frc.lib.input.ControllerManager;
+import frc.lib.utility.Settings;
+
 public class ArcadeDrive extends DriveSystem {
-
-
     public DriveSignal math(){
-        return null;
+        float throttle = ControllerManager.getInstance().getJoystickAxis(Settings.getInstance().drive_sticks[1][0], Settings.getInstance().drive_sticks[1][1]) - ControllerManager.getInstance().getJoystickAxis(Settings.getInstance().drive_sticks[0][0], Settings.getInstance().drive_sticks[0][1]);
+        float turn = ControllerManager.getInstance().getJoystickAxis(Settings.getInstance().drive_sticks[2][0], Settings.getInstance().drive_sticks[2][1]);
+
+        return new DriveSignal(throttle - turn,throttle + turn);
     }
 
     public void init() {
